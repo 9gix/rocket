211c2cb2de8a17a734701b899029e1436b2b5030
Diga Widyaprana <digawp@gmail.com>
2015-01-24 18:18:25 +0800

    Constructor and more API for rocket model

--- js/model/rocket.js
+++ js/model/rocket.js
@@ -1,23 +1,57 @@
 'use strict';
 
-function Rocket() {
+
+/**
+ * Rocket constructor
+ * @param x x-coordinate of rocket
+ * @param y y-coordinate of rocket
+ */
+function Rocket(x, y) {
 	this.velocity = 0;
 	this.acceleration = 0;
-	this.direction = UP;
+
+	this.direction = 0;
+
+	this._x = x;
+	this._y = y;
+
+	this.isThrottling = false;
+	this.isRotatingLeft = false;
+	this.isRotatingRight = false;
 }
 
-Rocket.prototype.thrust = function() {
-	// body...
+Rocket.prototype.throttle = function() {
+	this.isThrottling = true;
+};
+
+Rocket.prototype.stopThrottle = function() {
+	this.isThrottling = false;
 };
 
 Rocket.prototype.rotateLeft = function() {
-	
+	this.isRotatingLeft = true;
+};
+
+Rocket.prototype.stopRotateLeft = function() {
+	this.isRotatingLeft = false;
 };
 
 Rocket.prototype.rotateRight = function() {
-	// body...
+	this.isRotatingRight = true;
+};
+
+Rocket.prototype.stopRotateRight = function() {
+	this.isRotatingRight = false;
 };
 
 Rocket.prototype.update = function() {
-	// body...
+	if (this.isThrottling) {
+
+	}
+	if (this.isRotatingLeft) {
+
+	}
+	if (this.isRotatingRight) {
+
+	}
 };
