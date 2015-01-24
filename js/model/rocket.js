db1e4515706b68f815dec52283a4c9f08c7b51da
Diga Widyaprana <digawp@gmail.com>
2015-01-24 16:54:47 +0800

    Skeleton API for rocket model

---
+++ js/model/rocket.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function Rocket() {
+	this.velocity = 0;
+	this.acceleration = 0;
+	this.direction = UP;
+}
+
+Rocket.prototype.thrust = function() {
+	// body...
+};
+
+Rocket.prototype.rotateLeft = function() {
+	
+};
+
+Rocket.prototype.rotateRight = function() {
+	// body...
+};
+
+Rocket.prototype.update = function() {
+	// body...
+};
