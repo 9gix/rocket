2149444137de4fbe0d6a817fd0158af852f87b7c
Diga Widyaprana <digawp@gmail.com>
2015-01-25 00:27:19 +0800

    Forget thises

--- js/controllers/game.js
+++ js/controllers/game.js
@@ -5,14 +5,14 @@ function GameController(space){
 }
 
 GameController.prototype.throttle = function(){
-    space.rocket.throttle();
+    this.space.rocket.throttle();
 }
 
 GameController.prototype.rotateLeft = function(){
-    space.rocket.rotateLeft();
+    this.space.rocket.rotateLeft();
 }
 
 GameController.prototype.rotateRight = function(){
-    space.rocket.rotateRight();
+    this.space.rocket.rotateRight();
 
 }
