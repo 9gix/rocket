dd9faf68b36d3f641318d7d7d75fac1c2f231120
Diga Widyaprana <digawp@gmail.com>
2015-01-24 18:47:05 +0800

    Add more constants, rename some methods

--- js/model/rocket.js
+++ js/model/rocket.js
@@ -1,23 +1,32 @@
 'use strict';
 
-
 /**
  * Rocket constructor
  * @param x x-coordinate of rocket
  * @param y y-coordinate of rocket
+ * @param angle the angle in which the rocket is facing, defaults to 0
  */
-function Rocket(x, y) {
-	this.velocity = 0;
-	this.acceleration = 0;
+function Rocket(x, y, angle) {
+	// constants
+	this.thrust;
+	this.inertia;
+	this.manouvre;
+	this.maxSpeed;
 
-	this.direction = 0;
+	this.angle = angle || 0;
 
-	this._x = x;
-	this._y = y;
+	this.x = x;
+	this.y = y;
+
+	this.x_velocity = 0;
+	this.y_velocity = 0;
+
+	this.x_acceleration = 0;
+	this.y_acceleration = 0;
 
 	this.isThrottling = false;
-	this.isRotatingLeft = false;
-	this.isRotatingRight = false;
+	this.isRotatingCCW = false;
+	this.isRotatingCW = false;
 }
 
 Rocket.prototype.throttle = function() {
@@ -28,30 +37,30 @@ Rocket.prototype.stopThrottle = function() {
 	this.isThrottling = false;
 };
 
-Rocket.prototype.rotateLeft = function() {
-	this.isRotatingLeft = true;
+Rocket.prototype.rotateCCW = function() {
+	this.isRotatingCCW = true;
 };
 
-Rocket.prototype.stopRotateLeft = function() {
-	this.isRotatingLeft = false;
+Rocket.prototype.stopRotateCCW = function() {
+	this.isRotatingCCW = false;
 };
 
-Rocket.prototype.rotateRight = function() {
-	this.isRotatingRight = true;
+Rocket.prototype.rotateCW = function() {
+	this.isRotatingCW = true;
 };
 
-Rocket.prototype.stopRotateRight = function() {
-	this.isRotatingRight = false;
+Rocket.prototype.stopRotateCW = function() {
+	this.isRotatingCW = false;
 };
 
 Rocket.prototype.update = function() {
 	if (this.isThrottling) {
 
 	}
-	if (this.isRotatingLeft) {
+	if (this.isRotatingCCW) {
 
 	}
-	if (this.isRotatingRight) {
+	if (this.isRotatingCW) {
 
 	}
 };
