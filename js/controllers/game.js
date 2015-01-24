4dd926cb8e4e1e9a11e723e65aa90de66e8c096d
Eugene <yeo.eugene.oey@gmail.com>
2015-01-25 00:35:46 +0800

    Update Rocket Status

--- js/controllers/game.js
+++ js/controllers/game.js
@@ -6,13 +6,15 @@ function GameController(space){
 
 GameController.prototype.throttle = function(){
     this.space.rocket.throttle();
+    this.space.rocket.update();
 }
 
 GameController.prototype.rotateLeft = function(){
     this.space.rocket.rotateLeft();
+    this.space.rocket.update();
 }
 
 GameController.prototype.rotateRight = function(){
     this.space.rocket.rotateRight();
-
+    this.space.rocket.update();
 }
