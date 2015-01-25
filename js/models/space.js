f7f84a7f464db34fbea1d2ae3a264994c7affcc0
Diga Widyaprana <digawp@gmail.com>
2015-01-25 09:10:38 +0800

    Refactor inertia
    - Rename to drag
    - Move it out to space model
    - accountDrag is now called by space as well
    - update rocket model now only updates the coordinates (what updatePosition used to do)

--- js/models/space.js
+++ js/models/space.js
@@ -1,9 +1,11 @@
 function Space(rocket){
     this.rocket = rocket;
-    this.gravity = 0.52
+    this.gravity = 0.32;
+    this.drag = 0.21;
 }
 
 Space.prototype.update = function() {
 	this.rocket.accountGravity(this.gravity);
+	this.rocket.accountDrag(this.drag);
 	this.rocket.update();
 };
